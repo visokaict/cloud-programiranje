cf3cd130e2ac3ec4d6f60ceaa9f47740a46a60fb
Marko Spasojević <marko.spasojevic@nultien.rs>
2019-03-14 10:43:08 +0100

    Konfiguracija jsonwebtoken  i bcryptjs kreiranje rute za registraciju i generisanje tokena

--- RestAPI/JWTExample1/JWTExample1/app.js
+++ RestAPI/JWTExample1/JWTExample1/app.js
@@ -1,23 +1,48 @@
 "use strict";
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs'); 
 
 const app = express();
 const router = express.Router();
+
+var usersRepo = [];
+var idCounter = 0;
+const SECRET_KEY = "secretkey23456";
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.post('/register', (req, res) => {
-    res.status(200).send({ access_token: '' });
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = bcrypt.hashSync(req.body.password);
+
+    usersRepo.push({ "id":idCounter++, "name": name, "email": email, "password": password });
+    var lastElementIndex = usersRepo.length - 1;
+
+    const expiresIn = 24 * 60 * 60;
+    const accessToken = jwt.sign({ id: usersRepo[lastElementIndex].id }, SECRET_KEY, {
+        expiresIn: expiresIn
+    });
+
+    var responseObj = {
+        "user": usersRepo[lastElementIndex]["name"],
+        "access_token": accessToken,
+        "expires_in": expiresIn
+    };
+    res.status(200).send(responseObj);
 });
 
 router.post('/login', (req, res) => {
     res.status(200).send({ access_token: '' });
 });
+
 router.get('/', (req, res) => {
     res.status(200).send('This is an authentication server');
 });
+
 
 app.use(router);
 
